49b04c098cdf9056114bb9de4adeb622db5fa484
Ditte Gunterberg <ditte-g@users.noreply.github.com>
2017-03-24 17:26:53 +0100

    Changed isset

--- API/my_page_update_address.php
+++ API/my_page_update_address.php
@@ -1,15 +1,12 @@
 <?php
-
-// Uppdaterar användarens addressuppgifter (address, postnummer och postort) genom ett formulär på mina sidor
+/* Uppdaterar användarens addressuppgifter (address, postnummer och postort) genom ett formulär på mina sidor */
 require('db.php');
-include("auth.php");
+require("auth.php");
 $email = $_SESSION['email'];
 
-if( isset($_POST['faddress']) ) {
+if( isset($_POST['faddress'], $_POST['fzipcode'], $_POST['fcity']) ) {
     $sql = "UPDATE `users` SET `address` = :vaddress, `zipcode` = :vzipcode, `city` = :vcity WHERE `email` = :vemail";
     $stm_update = $pdo->prepare($sql);
     $stm_update->execute(['vaddress' => $_POST['faddress'], 'vzipcode' => $_POST['fzipcode'], 'vcity' => $_POST['fcity'], 'vemail' => $email]);
     echo json_encode(true);
     }
-
-?>
