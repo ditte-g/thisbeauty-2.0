cec9fddd4d64836829c559cb92bc7ece8c85c738
Ditte Gunterberg <ditte-g@users.noreply.github.com>
2017-03-27 13:48:32 +0200

    Changed array_push to $_SESSION['cart'][]

--- API/add_to_cart.php
+++ API/add_to_cart.php
@@ -2,32 +2,19 @@
 session_start();
 
 if (isset ($_GET['id'])) {
-  require 'db.php';
-
-    $stm_select = $pdo->prepare('SELECT * FROM products WHERE id='.$_GET['id']);
-    $stm_select->execute();
-    
-    foreach($stm_select as $row) {
-       
-        if(!empty($_SESSION['cart']))
-            if(is_array($_SESSION['cart']))
-            {
-                array_push($_SESSION['cart'], [ "item"=>$row, "quantity"=>1] ); //Om det redan finns varor i varukorgen så lägger man till den nya produkten i arrayen
+	require 'db.php';
+	
+	$stm_select = $pdo->prepare('SELECT * FROM products WHERE id='.$_GET['id']);
+	$stm_select->execute();
+	
+	foreach($stm_select as $row) {
+		
+		if(!empty($_SESSION['cart'])) {
+			$_SESSION['cart'][] = [ "item"=>$row, "quantity"=>1]; //Om det redan finns varor i varukorgen så lägger man till den nya produkten i arrayen
+		} else {
+			$_SESSION['cart'] = array( [ "item"=>$row, "quantity"=>1] ); //Om varukorgen är tom så skapas en array
             }
-            else
-            {
-                $_SESSION['cart'] = array( [ "item"=>$row, "quantity"=>1] ); //Om varukorgen är tom så skapas en array
-            }
-        else
-            $_SESSION['cart'] = array( [ "item"=>$row, "quantity"=>1] );
 
-        echo json_encode($_SESSION['cart']);
-
-        
-    }
-}   
-
-
-?>
-
-
+		echo json_encode($_SESSION['cart']);	
+	}
+}
