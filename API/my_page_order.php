612e48f5561b78e77ec38c7811c4da07c7dfa8a5
Ditte Gunterberg <ditte-g@users.noreply.github.com>
2017-03-27 14:35:30 +0200

    Added fetchAll(PDO::FETCH_ASSOC) instead of loop

--- API/my_page_order.php
+++ API/my_page_order.php
@@ -1,6 +1,6 @@
 <?php
 
-include("auth.php"); //include auth.php file on all secure pages
+require("auth.php"); //include auth.php file on all secure pages
 require('db.php');
 
 $email = $_SESSION['email'];
@@ -8,13 +8,7 @@ $email = $_SESSION['email'];
 //Visar användarens tidigare ordrar på mina sidor
 $stm_select = $pdo->prepare('SELECT * FROM `order` WHERE email = :email'); //Väljer den e-postadress som är inloggad
 $stm_select->execute(['email' => $email]);
-$resultat = array();
-
-foreach($stm_select as $row) {
-    $resultat[] = $row;
-
-}
+$result = $stm_select->fetchAll(PDO::FETCH_ASSOC);
 
 //JSON_UNESCAPED_UNICODE används för att kunna skriva ut åäö.
-echo json_encode($resultat, JSON_UNESCAPED_UNICODE);
-?>
+echo json_encode($result);
